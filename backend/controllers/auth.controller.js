3cbc31513173a063f4b29490c41c7479befca038
Belhrak Ali <140948858+nightitachi@users.noreply.github.com>
2024-09-24 12:43:07 +0100

    new

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -16,15 +16,19 @@ export const signup = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
     const newUser = new User({
       fullName,
       username,
       password: hashedPassword,
-      gender
+      gender,
+      profilePic : gender ==="male"? boyProfilePic :girlProfilePic
     });
 
     await newUser.save();
-
+    
     res.status(201).json({ message: 'User created successfully' });
 
   } catch (error) {
@@ -33,10 +37,18 @@ export const signup = async (req, res) => {
   }
 };
 
+
+
+
+//login Controller
 export const login = (req, res) => {
   console.log("loginUser");
 };
 
+
+
+
+//logout Controller
 export const logout = (req, res) => {
   console.log("logoutUser");
 };
