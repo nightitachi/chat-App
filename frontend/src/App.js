bb9a8e0307a6b6bebf6da3460d18ae811169b5a7
Belhrak Ali <140948858+nightitachi@users.noreply.github.com>
2024-11-04 20:27:39 +0100

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/login/Login.js';
-import Home from './pages/home/Home.js';
-import SignUp from './pages/signup/SignUp.js';
+import Home from './pages/home/home.js';
+import Login from './pages/login/login.js';
+import SignUp from './pages/signup/signup.js';
 import { useAuthContext } from './context/AuthContext.js';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const { authUser } = useAuthContext(); 
 
   return (
     <Router>
-      <div className="p-4 h-screen flex items-center justify-center">
+      <div className='p-4 h-screen flex items-center justify-center'>
         <Routes>
-          <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
-          <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
-          <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
+          <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+          <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+          <Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
         </Routes>
+        <Toaster />
       </div>
     </Router>
   );
